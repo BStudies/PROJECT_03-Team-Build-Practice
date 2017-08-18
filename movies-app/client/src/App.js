fc6c7afb353a874f60f170e73e308c8f53e6a7ac
Daniel Beebe <danwbeebe@gmail.com>
2017-08-18 12:27:30 -0400

    add code to app component

--- movies-app/client/src/App.js
+++ movies-app/client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import axios from 'axios';
@@ -8,19 +7,39 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 
+import Login from './components/Login';
+import Register from './components/Register';
+
+import MoviesList from './components/MoviesList';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-    auth: false,
-    user: null,
-    currentPage: 'home',
-    currentMovieId: null,
-    movieData: null,
+      auth: false,
+      user: null,
+      currentPage: 'home',
+      currentMovieId: null,
+      movieData: null,
+    }
+    this.setPage = this.setPage.bind(this);
+    this.handleMovieSubmit = this.handleMovieSubmit.bind(this);
+    this.handleMovieEditSubmit = this.handleMovieEditSubmit.bind(this);
+    this.selectEditedMovie = this.selectEditedMovie.bind(this);
   }
 
-  this.setPage = this.setPage.bind(this);
-}
+  // LIFECYCLE
+
+  componentDidMount() {
+    axios.get('/movies')
+      .then(res => {
+        this.setState({
+          movieData: res.data.data,
+        });
+      }).catch(err => console.log(err));
+  }
+
+  // PAGINATION
 
   setPage(page) {
     console.log('click');
@@ -34,27 +53,65 @@ class App extends Component {
       case 'home':
         return <Home />;
         break;
+      case 'movies':
+        return (<MoviesList
+          movieData={this.state.movieData}
+          handleMovieSubmit={this.handleMovieSubmit}
+          handleMovieEditSubmit={this.handleMovieEditSubmit}
+          selectEditedMovie={this.selectEditedMovie}
+          currentMovieId={this.state.currentMovieId}  />)
+        break;
       default:
         break;
     }
-    case ('movies'):
-    return <MoviesList movieData={this.state.movieData}
-    {/* there will be other things here too*/} />;
-    break;
   }
 
-  componentDidMount(){
-    axios.get('/movies ')
-    .then((res) => {
-      this.setState({
-      })
+  // MOVIES
+
+  handleMovieSubmit(e, title, description, genre) {
+    e.preventDefault();
+    axios.post('/movies', {
+      title,
+      description,
+      genre,
+    }).then(res => {
+      this.resetMovies();
+    }).catch(err => console.log(err));
+  }
+
+  handleMovieEditSubmit(e, title, description, genre) {
+    e.preventDefault();
+    axios.put(`/movies/${this.state.currentMovieId}`, {
+      title,
+      description,
+      genre,
+    }).then(res => {
+      this.resetMovies();
+    }).catch(err => console.log(err));
+  }
+
+  selectEditedMovie(id) {
+    this.setState({
+      currentMovieId: id,
     })
   }
+
+  resetMovies() {
+    axios.get('/movies')
+      .then(res => {
+        this.setState({
+          movieData: res.data.data,
+          currentMovieId: null,
+        })
+      }).catch(err => console.log(err));
+  }
+
+  // RENDER
 
   render() {
     return (
       <div className="App">
-        <Header setPage={this.setPage} />
+        <Header setPage={this.setPage} logOut={this.logOut} />
         {this.decideWhichPage()}
         <Footer />
       </div>
