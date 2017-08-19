9a8e41a6bef85dcfd110b847827bc39574c82ad2
Brandon <digihew@gmail.com>
2017-08-19 13:38:15 -0400

    remove excess movie routes

--- movies-app/routes/movie-routes.js
+++ movies-app/routes/movie-routes.js
@@ -7,18 +7,11 @@ movieRoutes.get('/', moviesController.index);           //handle /movies get
 movieRoutes.post('/', moviesController.create);         //handle /movies post
 
 
-//handle /movies/add
-movieRoutes.get('/add', function(req, res){
-    console.log('adding');
-    res.render('movies/movie-add', {
-        currentPage: 'add',
-    });
-});
+
 
 
 //handles /movies/id request verbs
 movieRoutes.get('/:id', moviesController.show);
-movieRoutes.get('/:id/edit', moviesController.edit);
 movieRoutes.put('/:id', moviesController.update);
 movieRoutes.delete('/:id',moviesController.delete);
 
